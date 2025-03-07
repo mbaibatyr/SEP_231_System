0e9fadf3323316dc0bc52bb8d611ccc8f336373c
mbaibatyr <murat_b@mail.ru>
2025-03-07 19:18:56 +0500

    Добавьте файлы проекта.

---
+++ SEP_231_System/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace SEP_231_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Process.GetProcesses();
+            foreach (var item in p)
+            {
+                Console.WriteLine($"{item.ProcessName}");
+            }
+        }
+    }
+}
