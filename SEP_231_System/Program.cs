8f31f2fd10563634e3cbdfff9dc5e1f1f6af3fef
mbaibatyr <murat_b@mail.ru>
2025-03-07 20:05:03 +0500

    +

--- SEP_231_System/Program.cs
+++ SEP_231_System/Program.cs
@@ -6,11 +6,31 @@ namespace SEP_231_System
     {
         static void Main(string[] args)
         {
-            var p = Process.GetProcesses();
-            foreach (var item in p)
-            {
-                Console.WriteLine($"{item.ProcessName}");
-            }
+            //var p = Process.GetProcesses()
+            //    .Where(z=>z.ProcessName == "notepad");
+            //foreach (var item in p)
+            //{                
+            //    Console.WriteLine($"{item.ProcessName}");
+            //}
+
+            //var p = from n in Process.GetProcesses()
+            //        where n.ProcessName == "notepad"
+            //        select n;
+            //    .Where(z=>z.ProcessName == "notepad");            
+
+            //var p = Process.GetProcessesByName("notepad");
+            //if (p != null && p.Length > 0)
+            //{
+            //    //Console.WriteLine($"{p[0].StartTime}");
+            //    foreach (ProcessModule item in p[0].Modules)
+            //    {
+            //        Console.WriteLine($"{item.FileName}");                    
+            //    }
+            //}
+
+            var p = Process.Start("notepad");
+            Console.WriteLine(p.Id);
+
         }
     }
 }
