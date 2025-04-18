71b43b5ed434f306ee22fd8232694b63598a0b0c
mbaibatyr <murat_b@mail.ru>
2025-04-18 21:34:29 +0500

    +

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string Name { get; }
+        public Class1(string name) => Name = name;
+        public Class1()
+        {
+            ;
+        }
+        public Class1(int i)
+        {
+            ;
+        }
+        public Class1(int i, string st)
+        {
+            ;
+        }
+
+        public void SayHello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+
+    public interface IClass1
+    {
+        int Method(string st);
+        Class1 Method2(string st);
+    }
+
+
+
+}
