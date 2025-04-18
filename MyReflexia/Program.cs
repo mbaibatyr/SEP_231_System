63c4efd54a4452d391624b493a91b4b132bcae5f
mbaibatyr <murat_b@mail.ru>
2025-04-18 19:40:41 +0500

    ++

---
+++ MyReflexia/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyReflexia
+{
+    public class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Type myType = typeof(Person);
+
+            //Console.WriteLine(myType.Assembly);
+            //Console.WriteLine(myType.Name);
+            //Console.WriteLine(myType.FullName);
+            Console.WriteLine($"Is class: {myType.IsClass}");
+
+            
+        }
+    }
+    public class Person
+    {
+        public string Name { get; }
+        public Person(string name) => Name = name;
+    }
+}
